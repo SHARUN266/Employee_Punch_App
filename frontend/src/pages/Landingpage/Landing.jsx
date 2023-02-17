3151c73101d80ff475c6380f90d3b0ee31ab0f7a
SHARUN266 <sharunattari266@gmail.com>
2023-02-17 22:00:17 +0530

    githubLandingPage

--- frontend/src/pages/Landingpage/Landing.jsx
+++ frontend/src/pages/Landingpage/Landing.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import Img from "../../Components/LazyloadingImg/Img";
+import "./style.scss";
+import logo from "../../assets/Logo.jpg";
+import video from "../../assets/LandingPage.mp4";
 export default function Landing() {
   return (
-    <div>Landing</div>
-  )
+    <div className="landing">
+      <div className="left">
+        <video className="video"  autoPlay  loop>
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+      <div className="right">
+        <Img className={"logo"} src={logo} />
+      </div>
+    </div>
+  );
 }
