8c97f77536d4df3cc559a08996ba4d484c36854a
SHARUN266 <sharunattari266@gmail.com>
2023-02-18 21:43:53 +0530

    landing_page

--- frontend/src/pages/Landingpage/Landing.jsx
+++ frontend/src/pages/Landingpage/Landing.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Img from "../../Components/LazyloadingImg/Img";
 import "./style.scss";
 import logo from "../../assets/Logo.jpg";
-import video from "../../assets/LandingPage.mp4";
+import video from "../../assets/creative-team.png";
+import { AwesomeButton } from "react-awesome-button";
+import 'react-awesome-button/dist/styles.css';
+import {GrFormNext} from "react-icons/gr"
 export default function Landing() {
   return (
     <div className="landing">
       <div className="left">
-        <video className="video"  autoPlay  loop>
-          <source src={video} type="video/mp4" />
-        </video>
+        <Img className={"logo"} src={logo} />
+       <Img className={"leftImg"} src={video}/>
       </div>
       <div className="right">
-        <Img className={"logo"} src={logo} />
+      <AwesomeButton size="large"   type="primary" after={<GrFormNext fontWeight={700} />}>Get Started</AwesomeButton>
       </div>
     </div>
   );
