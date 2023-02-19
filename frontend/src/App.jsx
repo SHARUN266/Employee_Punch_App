9111e9af173337f99c24f1b84a32a498e90ae5df
SHARUN266 <sharunattari266@gmail.com>
2023-02-19 11:48:53 +0530

    footer_add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Footer from './Components/Footer/Footer'
 import Landing from './pages/Landingpage/Landing'
 
 
@@ -6,6 +7,7 @@ function App() {
      return (
       <>
       <Landing/>
+      <Footer/>
       </>
      )
 }
--- frontend/src/pages/Landingpage/Landing.jsx
+++ frontend/src/pages/Landingpage/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Img from "../../Components/LazyloadingImg/Img";
 import "./style.scss";
-import logo from "../../assets/Logo.jpg";
+
 import video from "../../assets/creative-team.png";
 
 import "react-awesome-button/dist/styles.css";
