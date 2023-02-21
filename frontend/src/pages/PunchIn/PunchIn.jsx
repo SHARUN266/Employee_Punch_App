2268e7140176e8462926e85138b74e15c5167689
SHARUN266 <sharunattari266@gmail.com>
2023-02-21 15:11:29 +0530

    Geolocation_Add_Current

--- frontend/src/pages/PunchIn/PunchIn.jsx
+++ frontend/src/pages/PunchIn/PunchIn.jsx
@@ -1,20 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../Components/Button/Button";
 import ContentWrapper from "../../Components/contentWrapper/ContentWrapper";
 import { GetTime } from "../../Utils/GetTime";
 import "./style.scss";
+
 import GoogleMapReact from "google-map-react";
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 function PunchIn() {
+  
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
-
+ 
   const handleSubmit = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const location = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      setLocation(location);
+    });
     if (location) {
       // Handle form submission and store location data in database or backend
       console.log("Location:", location);
     } else {
-      setError("Please allow location access to record attendance.");
+      alert("Please allow location access to record attendance.");
     }
   };
 
@@ -32,6 +41,7 @@ function PunchIn() {
             map,
             title: "Your Location",
           });
+          
         },
         (error) => {
           setError(`Location access denied: ${error.message}`);
@@ -41,13 +51,7 @@ function PunchIn() {
       setError("Geolocation is not supported by this browser.");
     }
   };
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
+
   return (
     <ContentWrapper>
       <div className="container_login">
@@ -83,15 +87,23 @@ function PunchIn() {
           </div>
         </div>
         <div className="right">
-        <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyDm7D2xCvXv_byiQBw2fxPk9g-5BaJoa9k' }}
-        defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
-        defaultZoom={15}
-        yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-      >
-        {location && <AnyReactComponent lat={location.lat} lng={location.lng} text="You are here" />}
-      </GoogleMapReact>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyDm7D2xCvXv_byiQBw2fxPk9g-5BaJoa9k",
+            }}
+            center={{ lat: location?.lat||25.2165, lng: location?.lng||645.24565 }}
+            defaultZoom={15}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+          >
+            {location && (
+              <AnyReactComponent
+                lat={location.lat}
+                lng={location.lng}
+                text=" Hey Sharun! You are here"
+              />
+            )}
+          </GoogleMapReact>
           {/* <iframe
             src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2965.0824050173574!2d-93.63905729999999!3d41.998507000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sWebFilings%2C+University+Boulevard%2C+Ames%2C+IA!5e0!3m2!1sen!2sus!4v1390839289319"
             width="100%"
